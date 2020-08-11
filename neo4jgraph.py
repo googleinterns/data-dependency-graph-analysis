c228f88111c5dab6526aa915d4247084251061ef
Mariya Hirna <maryhirna@google.com>
2020-08-11 20:52:29 +0300

    Add env and data integrity for issue #2

--- neo4jgraph.py
+++ neo4jgraph.py
@@ -13,11 +13,13 @@ class Neo4jGraph:
     def run_query(self, query):
         # Function to allow run any cypher query
         with self.driver.session() as session:
-            session.write_transaction(self._run_query, query)
+            result = session.write_transaction(self._run_query, query)
+            for row in result:
+                print(row)
 
     @staticmethod
     def _run_query(tx, query):
-        tx.run(query)
+        return tx.run(query)
 
     def generate_dc(self, dc_id):
         with self.driver.session() as session:
@@ -67,19 +69,19 @@ class Neo4jGraph:
                 query=query, exception=exception))
             raise
 
-    def generate_dataset(self, d_id, dc_id):
+    def generate_dataset(self, d_id, dc_id, slo):
         with self.driver.session() as session:
-            result = session.write_transaction(self._generate_dataset, d_id, dc_id)
+            result = session.write_transaction(self._generate_dataset, d_id, dc_id, slo)
             for row in result:
                 print(f'Generated dataset {row["d"][0]} with regex grouping {row["d"][1]} and dataset collection {row["dc"]}')
 
     @staticmethod
-    def _generate_dataset(tx, d_id, dc_id):
+    def _generate_dataset(tx, d_id, dc_id, slo):
         # Generate a dataset node having a dataset id and dataset collection it belongs to
         query = (
-            f'MATCH (dc:dataset_collection) WHERE dc.collection_id = {dc_id} '
-            f'CREATE (d:dataset {{dataset_id: {d_id}, collection_id: {dc_id}, regex_grouping: "data.{d_id}.*", '
-            f'dataset_name: "dataset.{d_id}", dataset_description: "Dataset number {d_id}", slo: {53 + d_id}}}), '
+            f'MATCH (dc:dataset_collection) WHERE dc.dc_id = {dc_id} '
+            f'CREATE (d:dataset {{d_id: {d_id}, dc_id: {dc_id}, regex_grouping: "data.{d_id}.*", '
+            f'dataset_name: "dataset.{d_id}", dataset_description: "Dataset number {d_id}", slo: {slo}}}), '
             f'(dc) -[:CONTAINS] -> (d) '
             f'RETURN dc, d'
         )
@@ -96,19 +98,19 @@ class Neo4jGraph:
                 query=query, exception=exception))
             raise
 
-    def generate_system(self, s_id, sc_id):
+    def generate_system(self, s_id, s_critic, sc_id):
         with self.driver.session() as session:
-            result = session.write_transaction(self._generate_system, s_id, sc_id)
+            result = session.write_transaction(self._generate_system, s_id, sc_id, s_critic)
             for row in result:
                 print(f'Generated system {row["s"][0]} with regex grouping {row["s"][1]} and system collection {row["sc"]}')
 
     @staticmethod
-    def _generate_system(tx, s_id, sc_id):
+    def _generate_system(tx, s_id, sc_id, s_critic):
         # Generate a system node having a system id and system collection it belongs to
         query = (
             f'MATCH (sc:system_collection) WHERE sc.sc_id = {sc_id} '
             f'CREATE (s:system {{s_id: {s_id}, sc_id: {sc_id}, regex_grouping: "system.{s_id}.*", system_name: "system.{s_id}", '
-            f'system_description: "System number {s_id}", system_criticality: {24 + s_id}}}), '
+            f'system_description: "System number {s_id}", s_critic: {s_critic}}}), '
             f'(sc) -[:CONTAINS] -> (s) '
             f'RETURN sc, s'
         )
@@ -137,8 +139,8 @@ class Neo4jGraph:
         # Processing can either input or output the dataset
         query = (
             f'MATCH (s:system), (d:dataset) WHERE s.s_id = {s_id} AND d.d_id = {d_id} '
-            f'CREATE (p:processing {{p_id: {p_id}, impact: {impact}, freshness: {freshness}), '
-            f'(p) -[:{action}] - (d), (s) - [:CREATES] - (p) '
+            f'CREATE (p:processing {{p_id: {p_id}, impact: {impact}, freshness: {freshness}}}), '
+            f'(p) -[:{action}] -> (d), (s) - [:CREATES] -> (p) '
             f'RETURN d, s, p '
         )
 
@@ -148,6 +150,59 @@ class Neo4jGraph:
             return [{"d": row["d"]["d_id"],
                      "s": row["s"]["s_id"],
                      "p": row["p"]["p_id"]} for row in result]
+
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def generate_env(self, env_id, env_name, env_owner, env_oncall):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._generate_env, env_id, env_name, env_owner, env_oncall)
+            for row in result:
+                print(f'Generated environment {row["e"]}.')
+
+    @staticmethod
+    def _generate_env(tx, env_id, env_name, env_owner, env_oncall):
+        query = (
+            f'CREATE (e:environment {{env_id: {env_id}, name: "{env_name}", owner: "{env_owner}", oncall:"{env_oncall}"}}) '
+            f'RETURN e'
+        )
+
+        result = tx.run(query)
+
+        try:
+            return [{"e": row["e"]["env_id"]} for row in result]
+
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def generate_data_integrity(self, di_id, d_id, di_rec_time, di_volat, di_reg_time, di_rest_time):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._generate_data_integrity, di_id, d_id,
+                                               di_rec_time, di_volat, di_reg_time, di_rest_time)
+            for row in result:
+                print(f'Generated data integrity {row["d"]} for dataset {row["di"]}.')
+
+    @staticmethod
+    def _generate_data_integrity(tx, di_id, d_id, di_rec_time, di_volat, di_reg_time, di_rest_time):
+        query = (
+            f'MATCH (d:dataset) WHERE d.d_id = {d_id} '
+            f'CREATE (di:data_integrity {{di_id: {di_id}, di_rec_time: {di_rec_time}, '
+            f'di_volat: {di_volat}, di_reg_time: {di_reg_time}, di_rest_time: {di_rest_time} }}), '
+            f'(d) -[:has] -> (di) '
+            f'RETURN d, di '
+        )
+
+        result = tx.run(query)
+
+        try:
+            return [{"d": row["d"]["d_id"],
+                     "di": row["di"]["di_id"]} for row in result]
 
         # Capture any errors along with the query and data for traceability
         except ServiceUnavailable as exception:
