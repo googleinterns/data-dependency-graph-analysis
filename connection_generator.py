3c6ff674e399669ab33434448753269805cd9ca1
Mariya Hirna <maryhirna@google.com>
2020-09-15 16:27:32 +0300

    Minor fixes of docstrings #7

--- connection_generator.py
+++ connection_generator.py
@@ -1,6 +1,5 @@
 """
 This module implements methods for generating random connections between nodes in a graph.
-in a neo4j database.
 
 Method generate() will create all the necessary connections for the graph:
     dataset <-> dataset collection
@@ -21,21 +20,25 @@ class ConnectionGenerator:
 
     Attributes:
         dataset_count: Integer of how many datasets are in a graph.
-        dataset_count_map: Dictionary that maps number of datasets in collection to count of dataset collections.
+        dataset_count_map: Dictionary int:int that maps number of datasets in collection to count of its collections.
         system_count: Integer of how many systems are in a graph.
-        system_count_map: Dictionary that maps number of systems in collection to count of system collections.
+        system_count_map: Dictionary int:int that maps number of systems in collection to count of system collections.
         dataset_read_count: Integer of how many dataset reads are in a graph.
         dataset_write_count: Integer of how many dataset writes are in a graph.
         system_input_count: Integer of how many system inputs are in a graph.
         system_output_count: Integer of how many system outputs are in a graph.
-        dataset_read_count_map: Dictionary that maps number of system inputs of dataset read to count of dataset reads.
-        system_input_count_map: Dictionary that maps number of dataset reads by system input to count of system inputs.
-        dataset_write_count_map: Dictionary that maps number of system outputs of dataset write to count of dataset writes.
-        system_output_count_map: Dictionary that maps number of dataset writes by system output to count of system outputs.
-        datasets_conn_collection: Dictionary that maps dataset collection id to dataset ids.
-        systems_conn_collection: Dictionary that maps system collection id to system ids.
-        dataset_read_conn_systems: Dictionary that maps dataset read id to system ids this dataset inputs to.
-        dataset_write_conn_systems: Dictionary that maps dataset write id to system ids this dataset is output from.
+        dataset_read_count_map: Dictionary int:int that maps number of system inputs of dataset read to count of
+                                dataset reads.
+        system_input_count_map: Dictionary int:int that maps number of dataset reads by system input to count of
+                                system inputs.
+        dataset_write_count_map: Dictionary int:int that maps number of system outputs of dataset write to count of
+                                 dataset writes.
+        system_output_count_map: Dictionary int:int that maps number of dataset writes by system output to count of
+                                 system outputs.
+        datasets_conn_collection: Dictionary int:int that maps dataset collection id to dataset ids.
+        systems_conn_collection: Dictionary int:int that maps system collection id to system ids.
+        dataset_read_conn_systems: Dictionary int:int that maps dataset read id to system ids this dataset inputs to.
+        dataset_write_conn_systems: Dictionary int:int that maps dataset write id to system ids this dataset outputs from.
 
     Methods:
         get_one_to_many_connections()
@@ -93,24 +96,24 @@ class ConnectionGenerator:
 
         Args:
             element_count: Total number of elements.
-            element_count_map: Dictionary that maps element count in a group to number of groups with that count.
+            element_count_map: Dictionary int:int that maps element count in a group to number of groups with that count.
 
         Returns:
-            Dictionary that maps group id to a list of element ids.
+            Dictionary int:[int] that maps group id to a list of element ids.
         """
-        # Create element ids
+        # Create element ids.
         element_values = list(range(1, element_count + 1))
 
-        # Get number of elements for each group id from their count
+        # Get number of elements for each group id from their count.
         elements_per_group = [i for i in element_count_map for _ in range(element_count_map[i])]
 
-        # Randomise element ids and group ids
+        # Randomise element ids and group ids.
         random.shuffle(element_values)
         random.shuffle(elements_per_group)
 
         it = iter(element_values)
 
-        # Split element ids into chunks to get connections for each group
+        # Split element ids into chunks to get connections for each group.
         group_to_elements = {i + 1: set(islice(it, 0, elements_per_group[i])) for i in range(len(elements_per_group))}
 
         return group_to_elements
@@ -124,49 +127,49 @@ class ConnectionGenerator:
         Args:
             element_1_count: Total number of elements of type 1.
             element_2_count: Total number of elements of type 2.
-            element_1_count_map: Dictionary that maps element 1 count in element 2 group to number of elements 2.
-            element_2_count_map: Dictionary that maps element 2 count in element 1 group to number of elements 1.
+            element_1_count_map: Dictionary int:int that maps element 1 count in element 2 group to number of elements 2.
+            element_2_count_map: Dictionary int:int that maps element 2 count in element 1 group to number of elements 1.
 
         Returns:
             Dictionary that maps group 1 id to a list of group 2 ids.
         """
-        # Count zeros for each group
+        # Count zeros for each group.
         element_1_zeros = element_1_count_map[0] if 0 in element_1_count_map else 0
         element_2_zeros = element_2_count_map[0] if 0 in element_2_count_map else 0
 
-        # Create element ids
+        # Create element ids.
         element_1_values = list(range(1, element_1_count - element_1_zeros + 1))
         element_2_values = list(range(1, element_2_count - element_2_zeros + 1))
 
-        # Get number of elements in each group and remove groups with 0 elements
+        # Get number of elements in each group and remove groups with 0 elements.
         elements_per_group_1 = [i for i in element_1_count_map for j in range(element_1_count_map[i]) if i != 0]
         elements_per_group_2 = [i for i in element_2_count_map for j in range(element_2_count_map[i]) if i != 0]
         element_1_group_counter = {i + 1: elements_per_group_1[i] for i in range(len(elements_per_group_1))}
         element_2_group_counter = {i + 1: elements_per_group_2[i] for i in range(len(elements_per_group_2))}
 
-        # Create connection dictionary
+        # Create connection dictionary.
         element_1_conn_element_2 = {i: set() for i in element_1_values}
 
-        # Loop until run out of elements in any group
+        # Loop until any group runs out of elements.
         while element_1_values and element_2_values:
             # Generate a random connection
             element_1_gen = random.choice(element_1_values)
             element_2_gen = random.choice(element_2_values)
 
-            # Check if connection doesn't already exist
+            # Check if connection doesn't already exist.
             if not element_2_gen in element_1_conn_element_2[element_1_gen]:
-                # Add to existing connections and reduce count
+                # Add to existing connections and reduce count.
                 element_1_conn_element_2[element_1_gen].add(element_2_gen)
                 element_1_group_counter[element_1_gen] -= 1
                 element_2_group_counter[element_2_gen] -= 1
 
-                # If have all needed number of connections, remove id from possible options
+                # If have all needed number of connections, remove id from possible options.
                 if element_1_group_counter[element_1_gen] == 0:
                     element_1_values.remove(element_1_gen)
                 if element_2_group_counter[element_2_gen] == 0:
                     element_2_values.remove(element_2_gen)
 
-            # Check if all leftover elements aren't already included in this group
+            # Check if all leftover elements aren't already included in this group.
             elif set(element_2_values).issubset(element_1_conn_element_2[element_1_gen]):
                 element_1_values.remove(element_1_gen)
 
